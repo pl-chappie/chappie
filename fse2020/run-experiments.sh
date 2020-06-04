fd165359df4cc65110ae878853470fd1eea13971
pl-chappie <plchappie018@gmail.com>
2020-06-04 12:10:27 -0400

    testing change

--- fse2020/run-experiments.sh
+++ fse2020/run-experiments.sh
@@ -3,14 +3,11 @@
 dir=`dirname "$0"`
 work_dir=./chappie-data
 mkdir $work_dir
-mkdir $work_dir/plots
-echo "testing message" > $work_dir/plots/log.txt
-cat $work_dir/plots/log.txt
 
 # calmness experiment
-# $dir/experiments/calmness-experiment.sh $work_dir
-# python3 $dir/experiments/calmness-check.py -d $work_dir
+$dir/experiments/calmness-experiment.sh $work_dir
+python3 $dir/experiments/calmness-check.py -d $work_dir
 
 # profile experiment
-# $dir/experiments/profiling-experiment.sh $work_dir
-# $dir/plotting.sh $work_dir
+$dir/experiments/profiling-experiment.sh $work_dir
+$dir/plotting.sh $work_dir
